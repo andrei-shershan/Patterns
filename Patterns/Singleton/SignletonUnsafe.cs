787b223bd5f365b0f7e04d1521a39bfc328cf494
Andrei Shershan <mail.shershan@gmail.com>
2025-01-08 21:10:32 +0100

    Add thread-unsafe signleton

---
+++ Patterns/Singleton/SignletonUnsafe.cs
@@ -0,0 +1,23 @@
+﻿namespace Patterns.Patterns.Singleton
+{
+    public sealed class SignletonUnsafe
+    {
+        private static SignletonUnsafe? _instance;
+
+        private SignletonUnsafe() { }
+
+        public int Counter { get; set; }
+
+        public static SignletonUnsafe Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SignletonUnsafe();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ UseCases/SingletonCase.cs
@@ -0,0 +1,41 @@
+﻿using Patterns.Patterns.Singleton;
+
+namespace Patterns.UseCases
+{
+    internal class SingletonCase
+    {
+        public void Run()
+        {
+            var incrementor = new Incrementor();
+            var multiplier = new Multiplier();
+
+            incrementor.AddToCounter(10);
+            multiplier.Multiply(2);
+
+            if (SignletonUnsafe.Instance.Counter == 20)
+            {
+                System.Console.WriteLine("Singleton works, the counter is 20");
+            }
+            else
+            {
+                System.Console.WriteLine("Singleton failed, the counter is not 20");
+            }
+        }
+    }
+
+    internal class Incrementor
+    {
+        public void AddToCounter(int i)
+        {
+            SignletonUnsafe.Instance.Counter += i;
+        }
+    }
+
+    internal class Multiplier
+    {
+        public void Multiply(int i)
+        {
+            SignletonUnsafe.Instance.Counter *= i;
+        }
+    }
+}
