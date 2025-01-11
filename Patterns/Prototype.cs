1f7c170d906434cd67b6d26dd4bdb225f062f384
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-01-11 15:21:51 +0100

    Prototype (#6)

---
+++ Patterns/Prototype.cs
@@ -0,0 +1,50 @@
+﻿namespace Patterns.Patterns
+{
+    public interface IShape
+    {
+        IShape Clone();
+        void PrintDetails();
+    }
+
+    public class Rectangle : IShape
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public IShape Clone()
+        {
+            return new Rectangle(this.Width, this.Height);
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Rectangle: Width={Width}, Height={Height}");
+        }
+    }
+
+    public class Circle : IShape
+    {
+        public int Radius { get; set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public IShape Clone()
+        {
+            return new Circle(this.Radius);
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Circle: Radius={Radius}");
+        }
+    }
+}
---
+++ UseCases/PrototypeCase.cs
@@ -0,0 +1,34 @@
+﻿using Patterns.Patterns;
+
+namespace Patterns.UseCases
+{
+    internal class PrototypeCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(PrototypeCase));
+
+            IShape circle = new Circle(100);
+            IShape rectangle = new Rectangle(10, 20);
+
+            IShape clonedCircle = circle.Clone();
+            IShape clonedRectangle = rectangle.Clone();
+
+            circle.PrintDetails();
+            clonedCircle.PrintDetails();
+
+            CompareOpjects(nameof(Circle), circle, clonedCircle);
+
+            rectangle.PrintDetails();
+            clonedRectangle.PrintDetails();
+
+            CompareOpjects(nameof(Rectangle), circle, clonedCircle);
+        }
+
+        private void CompareOpjects(string objectName, object obj1, object obj2)
+        {
+            var comparisonResult = obj1 == obj2 ? "Same object" : "Different object";
+            Console.WriteLine($"{objectName}: {comparisonResult}");
+        }
+    }
+}
