9247e7b0bd945503e41e0387c2bfc8f04e38501f
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-01-22 20:34:02 +0100

    bridge (#8)
    * bridge
    
    * Fix naming

---
+++ Patterns/Bridge.cs
@@ -0,0 +1,61 @@
+﻿namespace Patterns.Patterns
+{
+    public interface IMessager
+    {
+        void SendMessage();
+    }
+
+    public class EmailMessager : IMessager
+    {
+        public void SendMessage()
+        {
+            System.Console.WriteLine("Email message sent");
+        }
+    }
+
+    public class SmsMessager : IMessager
+    {
+        public void SendMessage()
+        {
+            System.Console.WriteLine("SMS message sent");
+        }
+    }
+
+    public abstract class Notification
+    {
+        protected IMessager _messageger;
+
+        public Notification(IMessager messageger)
+        {
+            _messageger = messageger;
+        }
+
+        public abstract void Notify();
+    }
+
+    public class ReminderNotification : Notification
+    {
+        public ReminderNotification(IMessager messageger) : base(messageger)
+        {
+        }
+
+        public override void Notify()
+        {
+            Console.WriteLine("REMINDER");
+            _messageger.SendMessage();
+        }
+    }
+
+    public class AlertNotification : Notification
+    {
+        public AlertNotification(IMessager messageger) : base(messageger)
+        {
+        }
+
+        public override void Notify()
+        {
+            Console.WriteLine("ALERT");
+            _messageger.SendMessage();
+        }
+    }
+}
---
+++ UseCases/BridgeCase.cs
@@ -0,0 +1,27 @@
+﻿using Patterns.Patterns;
+
+namespace Patterns.UseCases
+{
+    public class BridgeCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(BridgeCase));
+
+            var emailMessage = new EmailMessager();
+            var smsMessage = new SmsMessager();
+
+            var reminderNotification = new ReminderNotification(emailMessage);
+            reminderNotification.Notify();
+
+            reminderNotification = new ReminderNotification(smsMessage);
+            reminderNotification.Notify();
+
+            var alertNotification = new AlertNotification(emailMessage);
+            alertNotification.Notify();
+
+            alertNotification = new AlertNotification(smsMessage);
+            alertNotification.Notify();
+        }
+    }
+}
