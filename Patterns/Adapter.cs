3d1106296a8f510b474b4a887f187279dd92b7d0
Andrei Shershan <mail.shershan@gmail.com>
2025-01-22 20:06:47 +0100

    adapter

---
+++ Patterns/Adapter.cs
@@ -0,0 +1,30 @@
+﻿namespace Patterns.Patterns
+{
+    public interface ITarget
+    {
+        void DoSmth();
+    }
+
+    public class LegacyAdaptee
+    {
+        public void LegacyJob()
+        {
+            Console.WriteLine("Some Legacy job here...");
+        }
+    }
+
+    public class TargetAdapter : ITarget
+    {
+        private readonly LegacyAdaptee _adaptee;
+
+        public TargetAdapter(LegacyAdaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public void DoSmth()
+        {
+            _adaptee.LegacyJob();
+        }
+    }
+}
