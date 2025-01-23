2be7efcd1d520401899f5e522b185c8b1361cfb2
Andrei Shershan <mail.shershan@gmail.com>
2025-01-23 22:56:03 +0100

    facade

---
+++ UseCases/FacadeCase.cs
@@ -0,0 +1,18 @@
+﻿using Patterns.Patterns;
+
+namespace Patterns.UseCases
+{
+    public class FacadeCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(FacadeCase));
+
+            var subsystemA = new SubsystemA();
+            var subsystemB = new SubsystemB();
+
+            var facade = new Facade(subsystemA, subsystemB);
+            facade.Operation();
+        }
+    }
+}
