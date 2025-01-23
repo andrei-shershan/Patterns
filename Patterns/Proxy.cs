7cff02fbbe1fe9bc601b8d4bea6b4f23c38a8485
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-01-23 23:36:10 +0100

    proxy (#12)

---
+++ Patterns/Proxy.cs
@@ -0,0 +1,55 @@
+﻿namespace Patterns.Patterns
+{
+    public interface IDataService
+    {
+        string GetData();
+    }
+
+    public class DatalService : IDataService
+    {
+        public string GetData()
+        {
+            return "Data from the server...";
+        }
+    }
+
+    public class CachedData
+    {
+        public string Value { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+
+
+    public class ProxyDataService : IDataService
+    {
+        private DatalService _dataService;
+        private readonly CachedData _cachedData = new CachedData();
+
+        private Dictionary<DateTime, string> _cacheEmulator = new Dictionary<DateTime, string>();
+
+        public string GetData()
+        {
+            var useCache = _cachedData.ExpirationDate > DateTime.Now;
+            if (useCache)
+            {
+                Console.WriteLine("Use cached value...");
+                return _cachedData.Value;
+            }
+            else
+            {
+                Console.WriteLine("Use real data...");
+                if (_dataService == null)
+                {
+                    _dataService = new DatalService();
+                }
+
+                var data = _dataService.GetData();
+
+                _cachedData.Value = data;
+                _cachedData.ExpirationDate = DateTime.Now.AddSeconds(3);
+
+                return data;
+            }
+        }
+    }
+}
