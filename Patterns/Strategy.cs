0ea7d473d3d739e2af561265feb9645ab56401e6
Andrei Shershan <mail.shershan@gmail.com>
2025-01-25 19:03:53 +0100

    strategy

---
+++ Patterns/Strategy.cs
@@ -0,0 +1,46 @@
+﻿namespace Patterns.Patterns
+{
+    namespace Strategy
+    {
+        public interface IStrategy
+        {
+            void FindInformation();
+        }
+
+        public class WebBrowser : IStrategy
+        {
+            public void FindInformation()
+            {
+                Console.WriteLine("Some data retreived from Google");
+            }
+        }
+
+        public class AiAgent : IStrategy
+        {
+            public void FindInformation()
+            {
+                Console.WriteLine("Some data retreived from AI");
+            }
+        }
+
+        public class Context
+        {
+            private IStrategy _strategy;
+
+            public Context()
+            {
+                
+            }
+
+            public void SetStrategy(IStrategy strategy)
+            {
+                _strategy = strategy;
+            }
+
+            public void DoSomething()
+            {
+                _strategy.FindInformation();
+            }
+        }
+    }
+}
