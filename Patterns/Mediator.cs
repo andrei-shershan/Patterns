18d788cb11fa89995485452d22103ad78b44ff33
Andrei Shershan <mail.shershan@gmail.com>
2025-01-25 13:00:23 +0100

    mediator

---
+++ Patterns/Mediator.cs
@@ -0,0 +1,66 @@
+﻿namespace Patterns.Patterns
+{
+    namespace Mediator
+    {
+        public interface IMediator
+        {
+            void SendMessageToMembers(object sender, string message);
+        }
+
+        public class ConcreteMediatore : IMediator
+        {
+            private readonly ICollection<BaseUser> _members = new List<BaseUser>();
+
+            public void AddMember(BaseUser member)
+            {
+                _members.Add(member);
+                member._mediator = this;
+            }
+
+            public void SendMessageToMembers(object sender, string message)
+            {
+                Console.WriteLine($"Message from {sender} to members: {message}");
+                foreach(var member in _members)
+                {
+                    if (member != sender)
+                    {
+                        member.Receive(message);
+                    }
+                }
+            }
+        }
+
+        public abstract class BaseUser
+        {
+            protected string _name;
+
+            public IMediator _mediator;
+
+            public BaseUser(string name)
+            {
+                _name = name;
+            }
+
+            public abstract void Send(string message);
+            public abstract void Receive(string message);
+        }
+
+        public class ConcreteUser : BaseUser
+        {
+            public ConcreteUser(string name) : base(name)
+            {
+            }
+
+            public override void Receive(string message)
+            {
+                Console.WriteLine($"{_name} received message: {message}");
+            }
+
+            public override void Send(string message)
+            {
+                Console.WriteLine($"{_name} sendind message: {message}");
+                _mediator?.SendMessageToMembers(this, message);
+            }
+        }
+    }
+}
---
+++ UseCases/MediatorCase.cs
@@ -0,0 +1,25 @@
+﻿using Patterns.Patterns.Mediator;
+
+namespace Patterns.UseCases
+{
+    public class MediatorCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(MediatorCase));
+
+            var userA = new ConcreteUser("User A");
+            var userB = new ConcreteUser("User B");
+            var userC = new ConcreteUser("User C");
+            var userD = new ConcreteUser("User D");
+
+            var mediator = new ConcreteMediatore();
+
+            mediator.AddMember(userA);
+            mediator.AddMember(userC);
+            mediator.AddMember(userD);
+
+            userA.Send("Hello, User C!");
+        }
+    }
+}
