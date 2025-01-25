7a886866cb90c41e5cfbab37ffa628cbf3698036
Andrei Shershan <mail.shershan@gmail.com>
2025-01-25 18:52:09 +0100

    fix conflicts

---
+++ UseCases/StateCase.cs
@@ -0,0 +1,22 @@
+﻿using Patterns.Patterns.State;
+
+namespace Patterns.UseCases
+{
+    public class StateCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(StateCase));
+
+            var document = new Document();
+
+            document.Create();
+            document.Save();
+
+            document.Create();
+            document.Publish();
+
+            document.Create();
+        }
+    }
+}
