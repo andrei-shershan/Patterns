bc55e4bdc42d32b45d04f722ab05ac442b6057cc
Andrei Shershan <mail.shershan@gmail.com>
2025-01-25 17:49:34 +0100

    memento

---
+++ Patterns/Memento.cs
@@ -0,0 +1,79 @@
+﻿namespace Patterns.Patterns
+{
+    namespace Memento
+    {
+        public class Cooridate
+        {
+            public long X { get; set; }
+
+            public long Y { get; set; }
+        }
+
+        public class DummyObject
+        {
+            private List<DummyMemento> _history = new List<DummyMemento>();
+
+            public string Name { get; private set; }
+
+            public Cooridate Cooridate { get; private set; }
+
+            public DummyObject(string name)
+            {
+                Name = name;
+                Cooridate = new Cooridate();
+            }
+
+            public void Rename(string newName)
+            {
+                Name = newName;
+            }
+
+            public void Move(long x, long y)
+            {
+                Cooridate.X += x;
+                Cooridate.Y += y;
+            }
+
+            public void GetStatus()
+            {
+                Console.WriteLine($"{Name} is on position x={Cooridate.X}, y={Cooridate.Y}");
+            }
+
+            public void SaveState()
+            {
+                _history.Add(new DummyMemento(this));
+            }
+
+            public void Restore()
+            {
+                var last = _history.LastOrDefault();
+                if(last != null)
+                {
+                    Name = last.Name;
+                    Cooridate = new Cooridate
+                    {
+                        X = last.Cooridate.X,
+                        Y = last.Cooridate.Y
+                    };
+                }
+            }
+        }
+
+        public class DummyMemento
+        {
+            public string Name { get; private set; }
+
+            public Cooridate Cooridate { get; private set; }
+
+            public DummyMemento(DummyObject dummyObject)
+            {
+                Name = dummyObject.Name;
+                Cooridate = new Cooridate
+                {
+                    X = dummyObject.Cooridate.X,
+                    Y = dummyObject.Cooridate.Y
+                };
+            }
+        }
+    }
+}
---
+++ UseCases/MementoCase.cs
@@ -0,0 +1,26 @@
+﻿using Patterns.Patterns.Memento;
+
+namespace Patterns.UseCases
+{
+    public class MementoCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(MementoCase));
+
+            var dummyObject = new DummyObject("first version");
+            dummyObject.Move(10, 20);
+            dummyObject.GetStatus();
+
+            dummyObject.SaveState();
+
+            dummyObject.Move(15, 25);
+            dummyObject.Rename("second version");
+
+            dummyObject.GetStatus();
+
+            dummyObject.Restore();
+            dummyObject.GetStatus();
+        }
+    }
+}
