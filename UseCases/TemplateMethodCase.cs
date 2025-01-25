d32410196dcbcb2a084d55a416ae2ed18beeed2f
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-01-25 19:15:59 +0100

    template method (#21)

---
+++ UseCases/TemplateMethodCase.cs
@@ -0,0 +1,18 @@
+﻿using Patterns.Patterns.TemplateMethod;
+
+namespace Patterns.UseCases
+{
+    public class TemplateMethodCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(TemplateMethodCase));
+
+            var dbProcessror = new DbProcessor();
+            dbProcessror.Process();
+
+            var localProcessor = new LocalProcessor();
+            localProcessor.Process();
+        }
+    }
+}
