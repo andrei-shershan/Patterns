9255442dad5e3615ea8bccf9798a73f75fb01080
Andrei Shershan <mail.shershan@gmail.com>
2025-01-25 10:48:19 +0100

    chain-of-responsobility

---
+++ UseCases/ChainOfResponsobilityCase.cs
@@ -0,0 +1,20 @@
+﻿using static Patterns.Patterns.Handler;
+
+namespace Patterns.UseCases
+{
+    public class ChainOfResponsobilityCase : IUseCase
+    {
+        public void Run()
+        {
+            Console.WriteLine(nameof(ChainOfResponsobilityCase));
+
+            var validationHandler = new ValidationHandler();
+            var saveDataHandler = new SaveDataHandler();
+            var loggerHandler = new LogHandler();
+
+            validationHandler.SetNext(saveDataHandler).SetNext(loggerHandler);
+
+            Console.WriteLine(validationHandler.Handle("Some data"));
+        }
+    }
+}
